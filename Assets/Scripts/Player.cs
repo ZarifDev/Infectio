b530b2d25d1135a78d47f7bcf5521053b9acf965
ZarifDev <62678750+ZarifDev@users.noreply.github.com>
2023-10-01 18:14:28 -0300

    barra de vida

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float vidaMaxima = 3;
+    public float vidaMaxima = 3; 
     public float vidaAtual = 3;
+
+
+    public Slider Slider;
+    public GameObject cubo;
+    public BoxCollider colisor;
+
     void Start()
     {
+      
         
     }
 
@@ -24,6 +32,14 @@ public class Player : MonoBehaviour
         if(vidaAtual + vidaParaCurar > vidaMaxima){
         vidaAtual = vidaMaxima;
         }
+    }
+    private void OnCollisionEnter(Collision algum_objeto) 
+    {
+        if(algum_objeto.gameObject.tag == "inimigo")
+        {
+            vidaAtual = vidaAtual -1;
+        }
+        
     }
     
 }
