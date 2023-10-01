25485b5e464ff616fb08cfa8a20cef0a2aa2ee3a
vantecc <138639389+vantecc@users.noreply.github.com>
2023-10-01 15:42:05 -0300

    Update ModuloVida.cs
    Limpeza de codigos em excesso.

--- Assets/Scripts/ModuloVida.cs
+++ Assets/Scripts/ModuloVida.cs
@@ -14,27 +14,3 @@ public class ModuloVida : MonoBehaviour
     {
         playerScript.vidaAtual = playerScript.vidaAtual + lifeRegen;
     }
-
-    // Update is called once per frame
-    /*
-    void Update()
-    {
-      
-    }
-
-
-    private void OnTriggerEnter(Collider other) //
-    {
-        GanharVida(other); //
-    }
-
-
-    public void GanharVida(Collider other) // metodos nao tem necessidade de ser minuscula;; gaveta
-    {
-        if (other.CompareTag("Player")) // se o obj colidido estiver com a tag "Player".
-        {
-            Destroy(this.gameObject);  // o que acontece se a tag "Player"
-        }
-    }
-    */
-}
