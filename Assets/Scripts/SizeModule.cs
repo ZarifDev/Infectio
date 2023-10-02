6653de0781d4ea4a7a30da5381bfad325da63ba3
LucasSG95 <145748141+LucasSG95@users.noreply.github.com>
2023-10-02 20:02:53 +0200

    Lucas
    speed, reload and size modules

---
+++ Assets/Scripts/SizeModule.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SizeModule : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "sizeMod")
+        {
+            this.transform.localScale *= 0.75f;
+            Destroy(other.gameObject);
+        }
+        // Start is called before the first frame update
+        void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
